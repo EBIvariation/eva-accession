505399724a79debb41b44b168225de187627b01c
jose miguel mut <jomutlo@gmail.com>
2018-04-05 14:18:48 +0100

    add VcfReaderConfiguration and use VcfReader from variation-commons

--- eva-accession-pipeline/src/main/java/uk/ac/ebi/eva/accession/pipeline/configuration/VcfReaderConfiguration.java
+++ eva-accession-pipeline/src/main/java/uk/ac/ebi/eva/accession/pipeline/configuration/VcfReaderConfiguration.java
@@ -15,5 +15,58 @@
  */
 package uk.ac.ebi.eva.accession.pipeline.configuration;
 
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.item.ItemStreamReader;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import uk.ac.ebi.eva.accession.pipeline.parameters.InputParameters;
+import uk.ac.ebi.eva.commons.batch.io.UnwindingItemStreamReader;
+import uk.ac.ebi.eva.commons.core.models.Aggregation;
+import uk.ac.ebi.eva.commons.core.models.pipeline.Variant;
+import uk.ac.ebi.eva.commons.batch.io.AggregatedVcfReader;
+import uk.ac.ebi.eva.commons.batch.io.VcfReader;
+
+import java.io.File;
+import java.io.IOException;
+
+
+/**
+ * Configuration to inject a VcfReader as a Variant Reader bean.
+ */
+@Configuration
 public class VcfReaderConfiguration {
+
+
+    private static final String VARIANT_READER = "VARIANT_READER";
+
+    @Bean(VARIANT_READER)
+    @StepScope
+    public ItemStreamReader<Variant> unwindingReader(VcfReader vcfReader) {
+        return new UnwindingItemStreamReader<>(vcfReader);
+    }
+
+    /**
+     * The aggregation type is passed so that spring won't cache the instance of VcfReader if it is already built
+     * with other aggregation type.
+     *
+     * @return a VcfReader for the given aggregation type.
+     * @throws IOException if the file doesn't exist, because it has to be read to see if it's compressed.
+     */
+    @Bean
+    @StepScope
+    public VcfReader vcfReader(InputParameters parameters) throws IOException {
+        String fileId = parameters.getVcfId();
+        String studyId = parameters.getStudyId();
+        File vcfFile = new File(parameters.getVcf());
+        Aggregation vcfAggregation = parameters.getVcfAggregation();
+
+        if (Aggregation.NONE.equals(vcfAggregation)) {
+            return new VcfReader(fileId, studyId, vcfFile);
+        } else {
+            return new AggregatedVcfReader(fileId, studyId, vcfAggregation, parameters.getAggregatedMappingFile(),
+                    vcfFile);
+        }
+    }
+
 }
