08829c82e58417e0724b2e796b5ef36f854901e8
Andres Silva <asilva@ebi.ac.uk>
2018-04-23 10:27:53 +0100

    Disabled the creation of accession in the API, using composition instead of inheritance in the controller. Changed the date to return a timestamp. Changed URL to submitted-variants. Added Swangger UI.

--- eva-accession-ws/src/main/java/uk/ac/ebi/eva/accession/ws/configuration/ApplicationConfiguration.java
+++ eva-accession-ws/src/main/java/uk/ac/ebi/eva/accession/ws/configuration/ApplicationConfiguration.java
@@ -17,13 +17,33 @@
  */
 package uk.ac.ebi.eva.accession.ws.configuration;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import uk.ac.ebi.ampt2d.commons.accession.rest.BasicRestController;
 
+import uk.ac.ebi.eva.accession.core.ISubmittedVariant;
+import uk.ac.ebi.eva.accession.core.SubmittedVariantAccessioningService;
 import uk.ac.ebi.eva.accession.core.configuration.SubmittedVariantAccessioningConfiguration;
+import uk.ac.ebi.eva.accession.ws.rest.SubmittedVariantDTO;
 
 @Configuration
 @Import({SubmittedVariantAccessioningConfiguration.class})
 public class ApplicationConfiguration {
 
+    @Bean
+    public ObjectMapper objectMapper(){
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        return objectMapper;
+    }
+
+    @Bean
+    public BasicRestController basicRestController(SubmittedVariantAccessioningService service) {
+        return new BasicRestController<ISubmittedVariant, SubmittedVariantDTO, Long>(service, SubmittedVariantDTO::new);
+    }
 }
