47914e9ea7c7a889c2a36e4b77a7db4f0a4ad9de
Andres Silva <asilva@ebi.ac.uk>
2018-04-24 14:00:01 +0100

    Added Api information to Swagger

--- eva-accession-ws/src/main/java/uk/ac/ebi/eva/accession/ws/configuration/SwaggerConfiguration.java
+++ eva-accession-ws/src/main/java/uk/ac/ebi/eva/accession/ws/configuration/SwaggerConfiguration.java
@@ -2,8 +2,11 @@ package uk.ac.ebi.eva.accession.ws.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -15,9 +18,22 @@ public class SwaggerConfiguration {
     @Bean
     public Docket productApi() {
         return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(getApiInfo())
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("uk.ac.ebi.eva.accession.ws.rest"))
                 .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo getApiInfo() {
+        return new ApiInfoBuilder()
+                .contact(new Contact("Europe Variation Archive - EMBL-EBI", "https://www.ebi.ac.uk/eva", null))
+                .license("Apache 2.0")
+                .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0")
+                .termsOfServiceUrl(null)
+                .title("EVA variant identifiers API")
+                .description("API to retrieve information about SubSNP (ss) and RefSNP (rs) variant identifiers")
+                .version("0.1")
                 .build();
     }
 }
