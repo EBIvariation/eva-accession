f4910d361224fd64bc78eaa33ff9e5b83f33e0a8
jose miguel mut <jomutlo@gmail.com>
2018-07-11 11:50:50 +0100

    add equals and hashcode to entities

--- eva-accession-core/src/main/java/uk/ac/ebi/eva/accession/core/persistence/SubmittedVariantEntity.java
+++ eva-accession-core/src/main/java/uk/ac/ebi/eva/accession/core/persistence/SubmittedVariantEntity.java
@@ -189,4 +189,69 @@ public class SubmittedVariantEntity extends AccessionedDocument<Long> implements
         return validated;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof SubmittedVariantEntity)) {
+            return false;
+        }
+
+        SubmittedVariantEntity that = (SubmittedVariantEntity) o;
+
+        if (taxonomyAccession != that.taxonomyAccession) {
+            return false;
+        }
+        if (start != that.start) {
+            return false;
+        }
+        if (!assemblyAccession.equals(that.assemblyAccession)) {
+            return false;
+        }
+        if (!projectAccession.equals(that.projectAccession)) {
+            return false;
+        }
+        if (!contig.equals(that.contig)) {
+            return false;
+        }
+        if (!referenceAllele.equals(that.referenceAllele)) {
+            return false;
+        }
+        if (!alternateAllele.equals(that.alternateAllele)) {
+            return false;
+        }
+        if (clusteredVariantAccession != null ? !clusteredVariantAccession.equals(
+                that.clusteredVariantAccession) : that.clusteredVariantAccession != null) {
+            return false;
+        }
+        if (supportedByEvidence != null ? !supportedByEvidence.equals(
+                that.supportedByEvidence) : that.supportedByEvidence != null) {
+            return false;
+        }
+        if (assemblyMatch != null ? !assemblyMatch.equals(that.assemblyMatch) : that.assemblyMatch != null) {
+            return false;
+        }
+        if (allelesMatch != null ? !allelesMatch.equals(that.allelesMatch) : that.allelesMatch != null) {
+            return false;
+        }
+        return validated != null ? validated.equals(that.validated) : that.validated == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = assemblyAccession.hashCode();
+        result = 31 * result + taxonomyAccession;
+        result = 31 * result + projectAccession.hashCode();
+        result = 31 * result + contig.hashCode();
+        result = 31 * result + (int) (start ^ (start >>> 32));
+        result = 31 * result + referenceAllele.hashCode();
+        result = 31 * result + alternateAllele.hashCode();
+        result = 31 * result + (clusteredVariantAccession != null ? clusteredVariantAccession.hashCode() : 0);
+        result = 31 * result + (supportedByEvidence != null ? supportedByEvidence.hashCode() : 0);
+        result = 31 * result + (assemblyMatch != null ? assemblyMatch.hashCode() : 0);
+        result = 31 * result + (allelesMatch != null ? allelesMatch.hashCode() : 0);
+        result = 31 * result + (validated != null ? validated.hashCode() : 0);
+        return result;
+    }
 }
