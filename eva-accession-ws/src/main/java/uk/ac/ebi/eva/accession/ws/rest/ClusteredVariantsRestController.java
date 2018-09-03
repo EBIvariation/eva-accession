0bdc3d79ba9c92365cf85ab8ad7243fdae1e277a
sundarvenkata <sundarvenkata@ebi.ac.uk>
2018-09-03 13:06:54 +0100

    Take out CrossOrigin imports in individual controllers

--- eva-accession-ws/src/main/java/uk/ac/ebi/eva/accession/ws/rest/ClusteredVariantsRestController.java
+++ eva-accession-ws/src/main/java/uk/ac/ebi/eva/accession/ws/rest/ClusteredVariantsRestController.java
@@ -19,7 +19,6 @@ package uk.ac.ebi.eva.accession.ws.rest;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
