44de6e020cba1626b28ecf30344ebffadb1f57e0
Pablo Arce Garcia <pablo.arce.garcia@gmail.com>
2018-09-14 12:14:50 +0100

    ClusteredVariantsRestControllerTest improvements

--- eva-accession-ws/src/test/java/uk/ac/ebi/eva/accession/ws/ClusteredVariantsRestControllerTest.java
+++ eva-accession-ws/src/test/java/uk/ac/ebi/eva/accession/ws/ClusteredVariantsRestControllerTest.java
@@ -32,6 +32,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
+import uk.ac.ebi.ampt2d.commons.accession.persistence.mongodb.document.AccessionedDocument;
 import uk.ac.ebi.ampt2d.commons.accession.rest.dto.AccessionResponseDTO;
 
 import uk.ac.ebi.eva.accession.core.ClusteredVariant;
@@ -53,6 +54,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Set;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import static junit.framework.TestCase.assertTrue;
@@ -66,22 +68,22 @@ public class ClusteredVariantsRestControllerTest {
 
     private static final String URL = "/v1/clustered-variants/";
 
-    private static final long CLUSTERED_VARIANT_ACCESSION_1 = 1L;
+    private static final long DBSNP_CLUSTERED_VARIANT_ACCESSION_1 = 1L;
 
-    private static final long CLUSTERED_VARIANT_ACCESSION_2 = 2L;
+    private static final long DBSNP_CLUSTERED_VARIANT_ACCESSION_2 = 2L;
 
-    private static final long SUBMITTED_VARIANT_ACCESSION_1 = 11L;
+    private static final long DBSNP_SUBMITTED_VARIANT_ACCESSION_1 = 11L;
 
-    private static final long SUBMITTED_VARIANT_ACCESSION_2 = 12L;
+    private static final long DBSNP_SUBMITTED_VARIANT_ACCESSION_2 = 12L;
 
-    private static final long SUBMITTED_VARIANT_ACCESSION_3 = 13L;
+    private static final long DBSNP_SUBMITTED_VARIANT_ACCESSION_3 = 13L;
 
     private static final int VERSION_1 = 1;
 
     private static final int VERSION_2 = 1;
 
     @Autowired
-    private DbsnpClusteredVariantAccessioningRepository repository;
+    private DbsnpClusteredVariantAccessioningRepository dbsnpRepository;
 
     @Autowired
     private DbsnpSubmittedVariantAccessioningRepository submittedVariantRepository;
@@ -102,53 +104,67 @@ public class ClusteredVariantsRestControllerTest {
 
     private DbsnpSubmittedVariantEntity submittedVariantEntity3;
 
+    private DbsnpClusteredVariantEntity clusteredVariantEntity1;
+
+    private DbsnpClusteredVariantEntity clusteredVariantEntity2;
+
     @Before
     public void setUp() {
-        repository.deleteAll();
+        setupDbSnpClusteredVariants();
+        setupDbsnpSubmittedVariants();
+    }
 
+    private void setupDbSnpClusteredVariants() {
         ClusteredVariant variant1 = new ClusteredVariant("ASMACC01", 1101, "CHROM1", 1234, VariantType.SNV, false);
         ClusteredVariant variant2 = new ClusteredVariant("ASMACC01", 1102, "CHROM1", 1234, VariantType.MNV, false);
 
         DbsnpClusteredVariantSummaryFunction function = new DbsnpClusteredVariantSummaryFunction();
-        DbsnpClusteredVariantEntity entity1 = new DbsnpClusteredVariantEntity(CLUSTERED_VARIANT_ACCESSION_1,
+        clusteredVariantEntity1 = new DbsnpClusteredVariantEntity(DBSNP_CLUSTERED_VARIANT_ACCESSION_1,
                                                                               function.apply(variant1), variant1);
-        DbsnpClusteredVariantEntity entity2 = new DbsnpClusteredVariantEntity(CLUSTERED_VARIANT_ACCESSION_2,
+        clusteredVariantEntity2 = new DbsnpClusteredVariantEntity(DBSNP_CLUSTERED_VARIANT_ACCESSION_2,
                                                                               function.apply(variant2), variant2);
-
-
-        SubmittedVariant submittedVariant1 = new SubmittedVariant("ASMACC01", 1101, "PROJECT1", "CHROM1", 1234, "REF",
-                                                                  "ALT", CLUSTERED_VARIANT_ACCESSION_1);
-        SubmittedVariant submittedVariant2 = new SubmittedVariant("ASMACC01", 1102, "PROJECT1", "CHROM1", 1234, "REF",
-                                                                  "ALT", CLUSTERED_VARIANT_ACCESSION_2);
-        SubmittedVariant submittedVariant3 = new SubmittedVariant("ASMACC01", 1102, "PROJECT2", "CHROM1", 1234, "REF",
-                                                                  "ALT", CLUSTERED_VARIANT_ACCESSION_2);
-
-        DbsnpSubmittedVariantSummaryFunction submittedVariantSummaryFunction =
-                new DbsnpSubmittedVariantSummaryFunction();
-        submittedVariantEntity1 =
-                new DbsnpSubmittedVariantEntity(SUBMITTED_VARIANT_ACCESSION_1,
-                                                submittedVariantSummaryFunction.apply(submittedVariant1),
-                                                submittedVariant1, VERSION_1);
-        submittedVariantEntity2 =
-                new DbsnpSubmittedVariantEntity(SUBMITTED_VARIANT_ACCESSION_2,
-                                                submittedVariantSummaryFunction.apply(submittedVariant2),
-                                                submittedVariant2, VERSION_1);
-        submittedVariantEntity3 =
-                new DbsnpSubmittedVariantEntity(SUBMITTED_VARIANT_ACCESSION_3,
-                                                submittedVariantSummaryFunction.apply(submittedVariant3),
-                                                submittedVariant2, VERSION_2);
 
         // No new dbSNP accessions can be generated, so the variants can only be stored directly using a repository
         // TODO When the support for new EVA accessions is implemented, this could be changed
         // In order to do so, replicate the structure of {@link SubmittedVariantsRestControllerTest}
-        generatedAccessions = repository.save(Arrays.asList(entity1, entity2));
+        generatedAccessions = dbsnpRepository.save(Arrays.asList(clusteredVariantEntity1, clusteredVariantEntity2));
+    }
+
+    private void setupDbsnpSubmittedVariants() {
+        // one variant has no default flags, while the others have the default values
+        SubmittedVariant submittedVariant1 = new SubmittedVariant("ASMACC01", 1101, "PROJECT1", "CHROM1", 1234, "REF",
+                                                                  "ALT", DBSNP_CLUSTERED_VARIANT_ACCESSION_1);
+        SubmittedVariant submittedVariant2 = new SubmittedVariant("ASMACC01", 1102, "PROJECT1", "CHROM1", 1234, "REF",
+                                                                  "ALT", DBSNP_CLUSTERED_VARIANT_ACCESSION_2);
+        SubmittedVariant submittedVariant3 = new SubmittedVariant("ASMACC01", 1102, "PROJECT2", "CHROM1", 1234, "REF",
+                                                                  "ALT", DBSNP_CLUSTERED_VARIANT_ACCESSION_2,
+                                                                  !ISubmittedVariant.DEFAULT_SUPPORTED_BY_EVIDENCE,
+                                                                  !ISubmittedVariant.DEFAULT_ASSEMBLY_MATCH,
+                                                                  !ISubmittedVariant.DEFAULT_ALLELES_MATCH,
+                                                                  !ISubmittedVariant.DEFAULT_VALIDATED);
+
+        DbsnpSubmittedVariantSummaryFunction submittedVariantSummaryFunction =
+                new DbsnpSubmittedVariantSummaryFunction();
+        submittedVariantEntity1 =
+                new DbsnpSubmittedVariantEntity(DBSNP_SUBMITTED_VARIANT_ACCESSION_1,
+                                                submittedVariantSummaryFunction.apply(submittedVariant1),
+                                                submittedVariant1, VERSION_1);
+        submittedVariantEntity2 =
+                new DbsnpSubmittedVariantEntity(DBSNP_SUBMITTED_VARIANT_ACCESSION_2,
+                                                submittedVariantSummaryFunction.apply(submittedVariant2),
+                                                submittedVariant2, VERSION_1);
+        submittedVariantEntity3 =
+                new DbsnpSubmittedVariantEntity(DBSNP_SUBMITTED_VARIANT_ACCESSION_3,
+                                                submittedVariantSummaryFunction.apply(submittedVariant3),
+                                                submittedVariant2, VERSION_2);
+
         generatedSubmittedVariantAccessions = submittedVariantRepository.save(
                 Arrays.asList(submittedVariantEntity1, submittedVariantEntity2, submittedVariantEntity3));
     }
 
     @After
     public void tearDown() {
-        repository.deleteAll();
+        dbsnpRepository.deleteAll();
         submittedVariantRepository.deleteAll();
     }
 
@@ -170,8 +186,18 @@ public class ClusteredVariantsRestControllerTest {
                                                                   Long>>>() {
                                           });
         assertEquals(HttpStatus.OK, getVariantsResponse.getStatusCode());
-        assertEquals(2, getVariantsResponse.getBody().size());
-        assertDefaultFlags(getVariantsResponse.getBody());
+        List<AccessionResponseDTO<ClusteredVariant, IClusteredVariant, String, Long>> wsResponseBody =
+                getVariantsResponse.getBody();
+        checkClusteredVariantsOutput(wsResponseBody);
+    }
+
+    private void checkClusteredVariantsOutput(
+            List<AccessionResponseDTO<ClusteredVariant, IClusteredVariant, String, Long>> getVariantsResponse) {
+        List<AccessionedDocument<IClusteredVariant, Long>> expectedVariants =
+                Arrays.asList(clusteredVariantEntity1, clusteredVariantEntity2);
+        assertVariantsAreContainedInControllerResponse(getVariantsResponse,
+                                                       expectedVariants,
+                                                       ClusteredVariant::new);
     }
 
     @Test
@@ -182,47 +208,50 @@ public class ClusteredVariantsRestControllerTest {
         List<AccessionResponseDTO<ClusteredVariant, IClusteredVariant, String, Long>> getVariantsResponse =
                 controller.get(identifiers);
 
-        assertEquals(2, getVariantsResponse.size());
-        assertDefaultFlags(getVariantsResponse);
+        checkClusteredVariantsOutput(getVariantsResponse);
     }
 
     @Test
-    public void testGetSubmittedVariants() {
-        getSubmittedVariantsByClusteredVariantIds(Collections.singletonList(CLUSTERED_VARIANT_ACCESSION_1),
-                                                  Collections.singletonList(submittedVariantEntity1));
-        getSubmittedVariantsByClusteredVariantIds(Collections.singletonList(CLUSTERED_VARIANT_ACCESSION_2),
-                                                  Arrays.asList(submittedVariantEntity2, submittedVariantEntity3));
-        getSubmittedVariantsByClusteredVariantIds(Arrays.asList(CLUSTERED_VARIANT_ACCESSION_1,
-                                                                CLUSTERED_VARIANT_ACCESSION_2),
-                                                  Arrays.asList(submittedVariantEntity1, submittedVariantEntity2,
-                                                                submittedVariantEntity3));
+    public void testGetSubmittedVariantsByClusteredVariantIds() {
+        getAndCheckSubmittedVariantsByClusteredVariantIds(
+                Collections.singletonList(DBSNP_CLUSTERED_VARIANT_ACCESSION_1),
+                Collections.singletonList(submittedVariantEntity1));
+        getAndCheckSubmittedVariantsByClusteredVariantIds(
+                Collections.singletonList(DBSNP_CLUSTERED_VARIANT_ACCESSION_2),
+                Arrays.asList(submittedVariantEntity2, submittedVariantEntity3));
+        getAndCheckSubmittedVariantsByClusteredVariantIds(
+                Arrays.asList(DBSNP_CLUSTERED_VARIANT_ACCESSION_1, DBSNP_CLUSTERED_VARIANT_ACCESSION_2),
+                Arrays.asList(submittedVariantEntity1, submittedVariantEntity2, submittedVariantEntity3));
     }
 
-    private void getSubmittedVariantsByClusteredVariantIds(List<Long> clusteredVariantIds,
-                                                           List<DbsnpSubmittedVariantEntity> expectedSubmittedVariants)
+    private void getAndCheckSubmittedVariantsByClusteredVariantIds(List<Long> clusteredVariantIds,
+                                                                   List<AccessionedDocument<ISubmittedVariant, Long>>
+                                                                   expectedSubmittedVariants)
     {
         List<AccessionResponseDTO<SubmittedVariant, ISubmittedVariant, String, Long>> getVariantsResponse =
                 controller.getSubmittedVariants(clusteredVariantIds);
-
-        assertEquals(expectedSubmittedVariants.size(), getVariantsResponse.size());
-
-        Set<Long> retrievedAccessions = getVariantsResponse.stream().map(AccessionResponseDTO::getAccession).collect(
-                Collectors.toSet());
-
-        assertTrue(expectedSubmittedVariants.stream().allMatch(
-                submittedVariant -> retrievedAccessions.contains(submittedVariant.getAccession())));
-
-        Set<SubmittedVariant> retrievedSubmitedVariants = getVariantsResponse.stream().map(
-                AccessionResponseDTO::getData).collect(Collectors.toSet());
-
-        assertTrue(expectedSubmittedVariants.stream().allMatch(
-                submittedVariant -> retrievedSubmitedVariants.contains(submittedVariant.getModel())));
-
+        assertVariantsAreContainedInControllerResponse(getVariantsResponse,
+                                                       expectedSubmittedVariants,
+                                                       SubmittedVariant::new);
     }
 
-    private void assertDefaultFlags(
-            List<AccessionResponseDTO<ClusteredVariant, IClusteredVariant, String, Long>> body) {
-        ClusteredVariant variant = body.get(0).getData();
-        assertEquals(IClusteredVariant.DEFAULT_VALIDATED, variant.isValidated());
+    private <DTO, MODEL> void assertVariantsAreContainedInControllerResponse(
+            List<AccessionResponseDTO<DTO, MODEL, String, Long>> getVariantsResponse,
+            List<AccessionedDocument<MODEL, Long>> expectedVariants,
+            Function<MODEL, DTO> modelToDto) {
+        // check the number of expected objects
+        assertEquals(expectedVariants.size(), getVariantsResponse.size());
+
+        // check the accessiones returned by the service
+        Set<Long> retrievedAccessions = getVariantsResponse.stream().map(AccessionResponseDTO::getAccession).collect(
+                Collectors.toSet());
+        assertTrue(expectedVariants.stream().allMatch(
+                expectedVariant -> retrievedAccessions.contains(expectedVariant.getAccession())));
+
+        // check the objects returned by the service
+        Set<DTO> variantsReturnedByController = getVariantsResponse.stream().map(
+                AccessionResponseDTO::getData).collect(Collectors.toSet());
+        assertTrue(expectedVariants.stream().map(AccessionedDocument::getModel).map(modelToDto).allMatch(
+                variantsReturnedByController::contains));
     }
 }
