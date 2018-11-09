4c4e28b6f043c74a8110285347a8eb92e74b226e
Sundar Venkataraman <sundarvenkata@ebi.ac.uk>
2018-11-09 16:36:20 +0000

    Addressed Andres' review comments

--- eva-accession-pipeline/src/main/java/uk/ac/ebi/eva/accession/pipeline/configuration/VariantProcessorConfiguration.java
+++ eva-accession-pipeline/src/main/java/uk/ac/ebi/eva/accession/pipeline/configuration/VariantProcessorConfiguration.java
@@ -1,13 +1,22 @@
 package uk.ac.ebi.eva.accession.pipeline.configuration;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.support.CompositeItemProcessor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import uk.ac.ebi.eva.accession.core.ISubmittedVariant;
 import uk.ac.ebi.eva.accession.pipeline.parameters.InputParameters;
+import uk.ac.ebi.eva.accession.pipeline.steps.processors.ExcludeStructuralVariantsProcessor;
 import uk.ac.ebi.eva.accession.pipeline.steps.processors.VariantProcessor;
+import uk.ac.ebi.eva.commons.core.models.IVariant;
 
-import static uk.ac.ebi.eva.accession.pipeline.configuration.BeanNames.VARIANT_PROCESSOR;
+import java.util.Arrays;
+
+import static uk.ac.ebi.eva.accession.pipeline.configuration.BeanNames.COMPOSITE_VARIANT_PROCESSOR;
 
 /**
  * Configuration to inject a VariantProcessor as a bean.
@@ -15,13 +24,30 @@ import static uk.ac.ebi.eva.accession.pipeline.configuration.BeanNames.VARIANT_P
 @Configuration
 public class VariantProcessorConfiguration {
 
-    @Bean(VARIANT_PROCESSOR)
+    private static final Logger logger = LoggerFactory.getLogger(VariantProcessorConfiguration.class);
+
+    @Bean(COMPOSITE_VARIANT_PROCESSOR)
     @StepScope
-    public VariantProcessor variantProcessor(InputParameters inputParameters) {
+    public ItemProcessor<IVariant, ISubmittedVariant> compositeVariantProcessor(
+            InputParameters inputParameters, VariantProcessor variantProcessor,
+            ExcludeStructuralVariantsProcessor excludeStructuralVariantsProcessor) {
+        logger.info("Injecting dbsnpVariantProcessor with parameters: {}", inputParameters);
+        CompositeItemProcessor<IVariant, ISubmittedVariant> compositeProcessor = new CompositeItemProcessor<>();
+        compositeProcessor.setDelegates(Arrays.asList(excludeStructuralVariantsProcessor, variantProcessor));
+        return compositeProcessor;
+    }
+
+    @Bean
+    VariantProcessor variantProcessor(InputParameters inputParameters) {
         String assemblyAccession = inputParameters.getAssemblyAccession();
         int taxonomyAccession = inputParameters.getTaxonomyAccession();
         String projectAccession = inputParameters.getProjectAccession();
 
         return new VariantProcessor(assemblyAccession, taxonomyAccession, projectAccession);
+    }
+
+    @Bean
+    ExcludeStructuralVariantsProcessor excludeStructuralVariantsProcessor() {
+        return new ExcludeStructuralVariantsProcessor();
     }
 }
--- eva-accession-pipeline/src/main/java/uk/ac/ebi/eva/accession/pipeline/configuration/jobs/steps/CreateSubsnpAccessionsStepConfiguration.java
+++ eva-accession-pipeline/src/main/java/uk/ac/ebi/eva/accession/pipeline/configuration/jobs/steps/CreateSubsnpAccessionsStepConfiguration.java
@@ -20,6 +20,7 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.step.tasklet.TaskletStep;
+import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.repeat.policy.SimpleCompletionPolicy;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,13 +31,12 @@ import org.springframework.context.annotation.Configuration;
 import uk.ac.ebi.eva.accession.core.ISubmittedVariant;
 import uk.ac.ebi.eva.accession.pipeline.io.AccessionWriter;
 import uk.ac.ebi.eva.accession.pipeline.policies.InvalidVariantSkipPolicy;
-import uk.ac.ebi.eva.accession.pipeline.steps.processors.VariantProcessor;
 import uk.ac.ebi.eva.commons.core.models.IVariant;
 import uk.ac.ebi.eva.commons.core.models.pipeline.Variant;
 
 import static uk.ac.ebi.eva.accession.pipeline.configuration.BeanNames.ACCESSION_WRITER;
 import static uk.ac.ebi.eva.accession.pipeline.configuration.BeanNames.CREATE_SUBSNP_ACCESSION_STEP;
-import static uk.ac.ebi.eva.accession.pipeline.configuration.BeanNames.VARIANT_PROCESSOR;
+import static uk.ac.ebi.eva.accession.pipeline.configuration.BeanNames.COMPOSITE_VARIANT_PROCESSOR;
 import static uk.ac.ebi.eva.accession.pipeline.configuration.BeanNames.VARIANT_READER;
 
 @Configuration
@@ -48,8 +48,8 @@ public class CreateSubsnpAccessionsStepConfiguration {
     private ItemReader<Variant> variantReader;
 
     @Autowired
-    @Qualifier(VARIANT_PROCESSOR)
-    private VariantProcessor variantProcessor;
+    @Qualifier(COMPOSITE_VARIANT_PROCESSOR)
+    private ItemProcessor<IVariant, ISubmittedVariant> variantProcessor;
 
     @Autowired
     @Qualifier(ACCESSION_WRITER)
