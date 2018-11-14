0ce71821e69ad14a9bab2dbead2cf02eab106247
jose miguel mut <jomutlo@gmail.com>
2018-11-14 15:09:03 +0000

    copyright fun

--- eva-accession-release/src/test/java/uk/ac/ebi/eva/accession/release/configuration/AccessionReleaseJobConfigurationTest.java
+++ eva-accession-release/src/test/java/uk/ac/ebi/eva/accession/release/configuration/AccessionReleaseJobConfigurationTest.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2018 EMBL - European Bioinformatics Institute
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package uk.ac.ebi.eva.accession.release.configuration;
 
 import com.lordofthejars.nosqlunit.annotation.UsingDataSet;
@@ -21,13 +37,7 @@ import uk.ac.ebi.eva.accession.release.test.configuration.MongoTestConfiguration
 import uk.ac.ebi.eva.accession.release.test.rule.FixSpringMongoDbRule;
 import uk.ac.ebi.eva.commons.core.utils.FileUtils;
 
-import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
