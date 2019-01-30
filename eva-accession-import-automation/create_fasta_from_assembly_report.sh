33614290fe30e7efe3ab0420fb094236179702ac
Cristina Yenyxe Gonzalez Garcia <cyenyxe@ebi.ac.uk>
2019-01-30 16:59:05 +0000

    Variable renamed for increased clarity

--- eva-accession-import-automation/create_fasta_from_assembly_report.sh
+++ eva-accession-import-automation/create_fasta_from_assembly_report.sh
@@ -12,31 +12,31 @@ assembly_accession=$1
 assembly_report=$2
 output_folder=$3
 
-for i in `grep -v -e "^#" ${assembly_report} | cut -f5`;
+for genbank_contig in `grep -v -e "^#" ${assembly_report} | cut -f5`;
 do
-    echo ${i}
+    echo ${genbank_contig}
 
     # Download each GenBank accession in the assembly report from ENA into a separate file
     # Delete the accession prefix from the header line
-    wget -q -O - "https://www.ebi.ac.uk/ena/data/view/${i}&display=fasta" | sed 's/ENA|.*|//g' > ${i}
+    wget -q -O - "https://www.ebi.ac.uk/ena/data/view/${genbank_contig}&display=fasta" | sed 's/ENA|.*|//g' > ${genbank_contig}
 
     # If a file has more than one line, then it is concatenated into the full assembly FASTA file
     # (empty sequences can't be indexed)
-    lines=`head -n 2 ${i} | wc -l`
+    lines=`head -n 2 ${genbank_contig} | wc -l`
     if [ $lines -ne 1 ]
     then
-        cat ${i} >> ${output_folder}/${assembly_accession}.fa
+        cat ${genbank_contig} >> ${output_folder}/${assembly_accession}.fa
     else
-        echo FASTA sequence not available for ${i}
+        echo FASTA sequence not available for ${genbank_contig}
     fi
 
     # Check that an accession is present no more than once in the output FASTA file, otherwise it 
     # means there are unexpected aliases in the assembly report, which need to be checked
-    acc=`head -n 1 ${i} | cut -f1 -d' ' | cut -f1 -d'.' | cut -f2 -d'>'`
+    acc=`head -n 1 ${genbank_contig} | cut -f1 -d' ' | cut -f1 -d'.' | cut -f2 -d'>'`
     matches=`grep -c "${acc}" ${output_folder}/${assembly_accession}.fa`
     if [ $matches -gt 1 ]
     then
-        echo WARNING: Sequence ${i} found more than once in the output FASTA file
+        echo WARNING: Sequence ${genbank_contig} found more than once in the output FASTA file
     fi
 done
 
