66519d4e37d6f2226ea6a12b47d0d7011542e0bc
Cristina Yenyxe Gonzalez Garcia <cyenyxe@ebi.ac.uk>
2019-01-30 16:15:14 +0000

    Prepare FASTA file with all INSDC sequences in an assembly report

---
+++ eva-accession-import-automation/create_fasta_from_assembly_report.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Usage: create_fasta_from_assembly_report.sh </path/to/assembly_report.txt>
+
+if [ "$#" -lt 3 ]
+then
+    echo "Please provide the assembly accession, the path to the assembly report and the output folder."
+    exit 1
+fi
+
+assembly_accession=$1
+assembly_report=$2
+output_folder=$3
+
+for i in `grep -v -e "^#" ${assembly_report} | cut -f5`;
+do
+    echo ${i}
+
+    # Download each GenBank accession in the assembly report from ENA into a separate file
+    # Delete the accession prefix from the header line
+    wget -q -O - "https://www.ebi.ac.uk/ena/data/view/${i}&display=fasta" | sed 's/ENA|.*|//g' > ${i}
+
+    # If a file has more than one line, then it is concatenated into the full assembly FASTA file
+    # (empty sequences can't be indexed)
+    lines=`wc -l ${i} | cut -f1 -d' '`
+    if [ $lines -ne 1 ]
+    then
+        cat ${i} >> ${output_folder}/${assembly_accession}.fa
+    else
+        echo FASTA sequence not available for ${i}
+    fi
+
+    # Check that an accession is present no more than once in the output FASTA file, otherwise it 
+    # means there are aliases in the assembly report
+    acc=`head -n 1 ${i} | cut -f1 -d' ' | cut -f1 -d'.' | cut -f2 -d'>'`
+    matches=`grep -c "${acc}" ${output_folder}/${assembly_accession}.fa`
+    if [ $matches -gt 1 ]
+    then
+        echo WARNING: Sequence ${i} found more than once in the output FASTA file
+    fi
+done
+
