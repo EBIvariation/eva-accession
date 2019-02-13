0a7cb02a6e7531c735453bd7af6a4d96dc6595c2
jose miguel mut <jomutlo@gmail.com>
2019-02-13 17:02:45 +0000

    remove the constraint of working correctly only for rat ¬¬

--- eva-accession-import-automation/create_fasta_from_assembly_report.sh
+++ eva-accession-import-automation/create_fasta_from_assembly_report.sh
@@ -48,7 +48,7 @@ do
     rm ${output_folder}/${genbank_contig}
 done
 
-echo `grep -v "^#" GCA_000001895.4_custom.txt  | wc -l` "contigs were present in the assembly report"
+echo `grep -v "^#" ${assembly_accession}  | wc -l` "contigs were present in the assembly report"
 echo `cat ${output_folder}/written_contigs.txt | wc -l` "contigs were successfully retrieved and written in the FASTA file"
 rm ${output_folder}/written_contigs.txt
 
