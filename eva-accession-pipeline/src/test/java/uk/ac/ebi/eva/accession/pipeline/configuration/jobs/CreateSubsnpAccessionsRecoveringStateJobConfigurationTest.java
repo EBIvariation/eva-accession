d7c985ce9f94b7fb410de15081740086b3bec8c2
jose miguel mut <jomutlo@gmail.com>
2019-06-04 13:17:17 +0100

    add failing test for recovering from uncommitted accessions

---
+++ eva-accession-pipeline/src/test/java/uk/ac/ebi/eva/accession/pipeline/configuration/jobs/CreateSubsnpAccessionsRecoveringStateJobConfigurationTest.java
@@ -0,0 +1,100 @@
+/*
+ * Copyright 2018 EMBL - European Bioinformatics Institute
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.ac.ebi.eva.accession.pipeline.configuration.jobs;
+
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.batch.core.BatchStatus;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.StepExecution;
+import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import uk.ac.ebi.eva.accession.core.configuration.SubmittedVariantAccessioningConfiguration;
+import uk.ac.ebi.eva.accession.core.persistence.SubmittedVariantAccessioningRepository;
+import uk.ac.ebi.eva.accession.core.persistence.SubmittedVariantEntity;
+import uk.ac.ebi.eva.accession.pipeline.parameters.InputParameters;
+import uk.ac.ebi.eva.accession.pipeline.test.BatchTestConfiguration;
+import uk.ac.ebi.eva.accession.pipeline.test.MongoTestConfiguration;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Collection;
+import java.util.Iterator;
+
+import static org.junit.Assert.assertEquals;
+import static uk.ac.ebi.eva.accession.pipeline.configuration.BeanNames.CHECK_SUBSNP_ACCESSION_STEP;
+import static uk.ac.ebi.eva.accession.pipeline.configuration.BeanNames.CREATE_SUBSNP_ACCESSION_STEP;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = {BatchTestConfiguration.class, SubmittedVariantAccessioningConfiguration.class,
+        MongoTestConfiguration.class})
+@TestPropertySource("classpath:accession-pipeline-recover-test.properties")
+public class CreateSubsnpAccessionsRecoveringStateJobConfigurationTest {
+
+    private static final int EXPECTED_VARIANTS = 22;
+
+    private static final long REUSED_ACCESSION = 5000000000L;
+
+    @Autowired
+    private JobLauncherTestUtils jobLauncherTestUtils;
+
+    @Autowired
+    private SubmittedVariantAccessioningRepository repository;
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    @Autowired
+    private InputParameters inputParameters;
+
+    @After
+    public void tearDown() throws Exception {
+        Files.deleteIfExists(Paths.get(inputParameters.getOutputVcf()));
+        Files.deleteIfExists(Paths.get(inputParameters.getFasta() + ".fai"));
+    }
+
+    @Test
+    public void accessionGeneratorShouldRecoverUncommitedAccessions() throws Exception {
+        writeSubmittedVariantWithUncommittedAccession(REUSED_ACCESSION);
+        assertEquals(1, repository.findByAccession(REUSED_ACCESSION).size());
+
+        JobExecution jobExecution = jobLauncherTestUtils.launchJob();
+        assertEquals(BatchStatus.COMPLETED, jobExecution.getStatus());
+        assertStepNames(jobExecution.getStepExecutions());
+
+        assertEquals(1, repository.findByAccession(REUSED_ACCESSION).size());
+    }
+
+    private void writeSubmittedVariantWithUncommittedAccession(long accession) {
+        mongoTemplate.insert(
+                new SubmittedVariantEntity(accession, "hash-1", "GCA_x", 9999, "project_id", "contig_id", 100,
+                                           "A", "T", null, false, false, false, false, 1));
+    }
+
+    private void assertStepNames(Collection<StepExecution> stepExecutions) {
+        assertEquals(2, stepExecutions.size());
+        Iterator<StepExecution> iterator = stepExecutions.iterator();
+        assertEquals(CREATE_SUBSNP_ACCESSION_STEP, iterator.next().getStepName());
+        assertEquals(CHECK_SUBSNP_ACCESSION_STEP, iterator.next().getStepName());
+    }
+}
