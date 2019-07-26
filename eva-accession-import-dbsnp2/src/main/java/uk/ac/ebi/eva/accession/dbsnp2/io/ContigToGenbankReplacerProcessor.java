399a6598cad2f725e7eaba23e853ead78052afc5
Rohan Kumar <108krohan@gmail.com>
2019-07-26 12:59:12 +0530

    Code clean up, update paths in application properties

--- eva-accession-import-dbsnp2/src/main/java/uk/ac/ebi/eva/accession/dbsnp2/io/ContigToGenbankReplacerProcessor.java
+++ eva-accession-import-dbsnp2/src/main/java/uk/ac/ebi/eva/accession/dbsnp2/io/ContigToGenbankReplacerProcessor.java
@@ -15,7 +15,6 @@
  */
 package uk.ac.ebi.eva.accession.dbsnp2.io;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.batch.item.ItemProcessor;
