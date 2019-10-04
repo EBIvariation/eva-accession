179aa969c70800915f00e184930bbeb866f3b672
Andres Silva <afsilva04@gmail.com>
2019-10-04 15:55:05 +0100

    remove unused import

--- eva-accession-release/src/test/java/uk/ac/ebi/eva/accession/release/configuration/steps/CreateReleaseStepConfigurationTest.java
+++ eva-accession-release/src/test/java/uk/ac/ebi/eva/accession/release/configuration/steps/CreateReleaseStepConfigurationTest.java
@@ -41,7 +41,6 @@ import uk.ac.ebi.eva.commons.core.utils.FileUtils;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
