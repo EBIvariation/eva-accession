256511e21614118992ec9c91be936f9d542a17ad
Andres Silva <afsilva04@gmail.com>
2020-03-09 14:41:16 +0000

    added test data to be loaded in tests

--- eva-accession-clustering/src/test/java/uk/ac/ebi/eva/accession/clustering/configuration/batch/steps/ClusteringVariantStepConfigurationTest.java
+++ eva-accession-clustering/src/test/java/uk/ac/ebi/eva/accession/clustering/configuration/batch/steps/ClusteringVariantStepConfigurationTest.java
@@ -15,13 +15,19 @@
  */
 package uk.ac.ebi.eva.accession.clustering.configuration.batch.steps;
 
+import com.lordofthejars.nosqlunit.annotation.UsingDataSet;
+import com.lordofthejars.nosqlunit.mongodb.MongoDbConfigurationBuilder;
+import com.lordofthejars.nosqlunit.mongodb.MongoDbRule;
+import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.batch.core.BatchStatus;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.test.JobLauncherTestUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
@@ -29,7 +35,8 @@ import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import uk.ac.ebi.eva.accession.clustering.parameters.InputParameters;
-import uk.ac.ebi.eva.accession.clustering.test.BatchTestConfiguration;
+import uk.ac.ebi.eva.accession.clustering.test.configuration.BatchTestConfiguration;
+import uk.ac.ebi.eva.accession.clustering.test.rule.FixSpringMongoDbRule;
 import uk.ac.ebi.eva.accession.core.model.eva.SubmittedVariantEntity;
 
 import static org.junit.Assert.assertEquals;
@@ -49,13 +56,27 @@ public class ClusteringVariantStepConfigurationTest {
     @Autowired
     private MongoTemplate mongoTemplate;
 
+    //Required by nosql-unit
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    @Rule
+    public MongoDbRule mongoDbRule = new FixSpringMongoDbRule(
+            MongoDbConfigurationBuilder.mongoDb().databaseName("test-db").build());
+
     @Before
     public void setUp() {
         mongoTemplate.dropCollection(SubmittedVariantEntity.class);
     }
 
+    @After
+    public void tearDown() {
+        mongoTemplate.dropCollection(SubmittedVariantEntity.class);
+    }
+
     @Test
     @DirtiesContext
+    @UsingDataSet(locations = {"/test-data/submittedVariantEntity.json"})
     public void test() {
         JobExecution jobExecution = jobLauncherTestUtils.launchStep(CLUSTERING_STEP);
         assertEquals(BatchStatus.COMPLETED, jobExecution.getStatus());
