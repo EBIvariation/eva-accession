f11af34a6fc189f14d51388d96f0c4f109ab5edf
jose miguel mut <jomutlo@gmail.com>
2020-07-10 23:26:17 +0100

    fix processor tests (also put extra care on exception messages)

--- eva-accession-clustering/src/main/java/uk/ac/ebi/eva/accession/clustering/batch/processors/VariantToSubmittedVariantEntityProcessor.java
+++ eva-accession-clustering/src/main/java/uk/ac/ebi/eva/accession/clustering/batch/processors/VariantToSubmittedVariantEntityProcessor.java
@@ -23,44 +23,68 @@ import uk.ac.ebi.eva.accession.core.model.ISubmittedVariant;
 import uk.ac.ebi.eva.accession.core.model.SubmittedVariant;
 import uk.ac.ebi.eva.accession.core.model.eva.SubmittedVariantEntity;
 import uk.ac.ebi.eva.accession.core.summary.SubmittedVariantSummaryFunction;
+import uk.ac.ebi.eva.accession.remapping.batch.io.VariantContextWriter;
+import uk.ac.ebi.eva.accession.remapping.batch.processors.SubmittedVariantToVariantContextProcessor;
 import uk.ac.ebi.eva.commons.core.models.pipeline.Variant;
+import uk.ac.ebi.eva.commons.core.models.pipeline.VariantSourceEntry;
 
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class VariantToSubmittedVariantEntityProcessor implements ItemProcessor<Variant, SubmittedVariantEntity> {
 
     private String assemblyAccession;
 
-    private String projectAccession;
-
     private Function<ISubmittedVariant, String> hashingFunction;
 
-    public VariantToSubmittedVariantEntityProcessor(String assemblyAccession, String projectAccession) {
+    public VariantToSubmittedVariantEntityProcessor(String assemblyAccession) {
         if (assemblyAccession == null) {
             throw new IllegalArgumentException("assembly accession must be provided when reading from a VCF");
         }
-        if (projectAccession == null) {
-            throw new IllegalArgumentException("project accession must be provided when reading from a VCF");
-        }
         this.assemblyAccession = assemblyAccession;
-        this.projectAccession = projectAccession;
         hashingFunction = new SubmittedVariantSummaryFunction().andThen(new SHA1HashingFunction());
     }
 
     @Override
     public SubmittedVariantEntity process(Variant variant) {
         Assert.isTrue(variant.getMainId() != null && variant.getMainId().matches("ss[0-9]+"),
-                      "The variants should have an SS ID as main ID: " + variant.toString());
+                      "All variants should have an SS ID as main ID: variant without ID: " + variant.toString());
+        Assert.isTrue(variant.getSourceEntries().size() == 1,
+                      "All variants should have just 1 source entry (each variant should come from a single VCF). " +
+                              "SourceEntries found: [" + variant.getSourceEntries().stream()
+                                                                .map(e -> e.getAttributes().toString())
+                                                                .collect(Collectors.joining(", ")) + "]"
+                              + " in variant " + variant.toString());
         long accession = Long.parseLong(variant.getMainId().substring(2));
+        VariantSourceEntry sourceEntry = variant.getSourceEntries().iterator().next();
+
+        Long rs = parseRs(sourceEntry.getAttribute(VariantContextWriter.RS_KEY));
+
+        String projectAccession = sourceEntry.getAttribute(VariantContextWriter.PROJECT_KEY);
+        Assert.hasText(projectAccession, "project accession should be provided for all variants. Missing in variant "
+                + variant.toString());
 
         SubmittedVariant submittedVariant = new SubmittedVariant(assemblyAccession, 0, projectAccession,
                                                                  variant.getChromosome(), variant.getStart(),
-                                                                 variant.getReference(), variant.getAlternate(), null);
+                                                                 variant.getReference(), variant.getAlternate(), rs);
 
         String hash = hashingFunction.apply(submittedVariant);
 
         SubmittedVariantEntity submittedVariantEntity = new SubmittedVariantEntity(accession, hash,
                                                                                    submittedVariant, 1);
         return submittedVariantEntity;
+    }
+
+    private Long parseRs(String rsString) {
+        Long rs = null;
+        if (rsString != null) {
+            if (!rsString.startsWith(SubmittedVariantToVariantContextProcessor.RS_PREFIX)) {
+                throw new IllegalArgumentException(
+                        "if the rs attribute is present (INFO column in VCF), it should be in the format 'rs[0-9]+'. " +
+                                "Found: '" + rsString + "'");
+            }
+            rs = Long.valueOf(rsString.substring(2));
+        }
+        return rs;
     }
 }
