8bf6f26962fe6eef52f843066b01885731d107de
Andres Silva <afsilva04@gmail.com>
2021-05-02 13:26:26 +0100

    change module name from eva-ingest-remapped to eva-remapping-ingest

---
+++ eva-remapping-ingest/src/main/java/uk/ac/ebi/eva/remapping/ingest/batch/io/RemappedSubmittedVariantsWriter.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2021 EMBL - European Bioinformatics Institute
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.ac.ebi.eva.remapping.ingest.batch.io;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.data.mongodb.core.BulkOperations;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+import uk.ac.ebi.eva.accession.core.model.eva.SubmittedVariantEntity;
+import uk.ac.ebi.eva.remapping.ingest.configuration.BeanNames;
+
+import java.util.List;
+
+public class RemappedSubmittedVariantsWriter implements ItemWriter<SubmittedVariantEntity> {
+
+    private static final Logger logger = LoggerFactory.getLogger(RemappedSubmittedVariantsWriter.class);
+
+    private MongoTemplate mongoTemplate;
+
+    public RemappedSubmittedVariantsWriter(MongoTemplate mongoTemplate) {
+        this.mongoTemplate = mongoTemplate;
+    }
+
+    @Override
+    public void write(List<? extends SubmittedVariantEntity> submittedVariantsRemapped) throws Exception {
+        try {
+            BulkOperations bulkOperations = mongoTemplate.bulkOps(BulkOperations.BulkMode.UNORDERED,
+                                                                  SubmittedVariantEntity.class,
+                                                                  BeanNames.SUBMITTED_VARIANT_ENTITY);
+            bulkOperations.insert(submittedVariantsRemapped);
+            bulkOperations.execute();
+        } catch (DuplicateKeyException exception) {
+            logger.warn(exception.toString());
+        }
+    }
+}
---
+++ eva-remapping-ingest/src/main/java/uk/ac/ebi/eva/remapping/ingest/batch/processors/VariantToSubmittedVariantEntityRemappedProcessor.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2021 EMBL - European Bioinformatics Institute
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.ac.ebi.eva.remapping.ingest.batch.processors;
+
+import org.apache.commons.lang3.math.NumberUtils;
+import org.springframework.batch.item.ItemProcessor;
+import uk.ac.ebi.ampt2d.commons.accession.hashing.SHA1HashingFunction;
+
+import uk.ac.ebi.eva.accession.core.model.ISubmittedVariant;
+import uk.ac.ebi.eva.accession.core.model.SubmittedVariant;
+import uk.ac.ebi.eva.accession.core.model.eva.SubmittedVariantEntity;
+import uk.ac.ebi.eva.accession.core.summary.SubmittedVariantSummaryFunction;
+import uk.ac.ebi.eva.commons.core.models.pipeline.Variant;
+import uk.ac.ebi.eva.commons.core.models.pipeline.VariantSourceEntry;
+
+import java.util.function.Function;
+
+public class VariantToSubmittedVariantEntityRemappedProcessor implements ItemProcessor<Variant,
+        SubmittedVariantEntity> {
+
+    public static final String PROJECT_KEY = "PROJECT";
+
+    public static final String RS_KEY = "RS";
+
+    private String assemblyAccession;
+
+    private int taxonomyAccession;
+
+    private String remappedFrom;
+
+    private Function<ISubmittedVariant, String> hashingFunction;
+
+    public VariantToSubmittedVariantEntityRemappedProcessor(String assemblyAccession, int taxonomyAccession,
+                                                            String remappedFrom) {
+        if (assemblyAccession == null || remappedFrom == null) {
+            throw new IllegalArgumentException("assembly accession and assembly remapped from must be provided");
+        }
+        this.assemblyAccession = assemblyAccession;
+        this.taxonomyAccession = taxonomyAccession;
+        this.remappedFrom = remappedFrom;
+        hashingFunction = new SubmittedVariantSummaryFunction().andThen(new SHA1HashingFunction());
+    }
+
+    @Override
+    public SubmittedVariantEntity process(Variant variant) throws Exception {
+        long accession = Long.parseLong(variant.getMainId().substring(2));
+        VariantSourceEntry sourceEntry = variant.getSourceEntries().iterator().next();
+
+        String projectAccession = sourceEntry.getAttribute(PROJECT_KEY);
+        Long rsId = NumberUtils.createLong(sourceEntry.getAttribute(RS_KEY).substring(2));
+
+        SubmittedVariant submittedVariant = new SubmittedVariant(assemblyAccession, taxonomyAccession, projectAccession,
+                                                                 variant.getChromosome(), variant.getStart(),
+                                                                 variant.getReference(), variant.getAlternate(), rsId);
+
+        String hash = hashingFunction.apply(submittedVariant);
+        SubmittedVariantEntity submittedVariantRemappedEntity = new SubmittedVariantEntity(accession, hash,
+                                                                                           submittedVariant, 1,
+                                                                                           remappedFrom);
+        return submittedVariantRemappedEntity;
+    }
+}
