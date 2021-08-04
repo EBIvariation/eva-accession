019540ffb14057a38bd4e0a408a013ea786f71f9
nitin-ebi <79518737+nitin-ebi@users.noreply.github.com>
2021-08-04 18:13:30 +0530

    EVA-2416: Store Clustering count in DB through count-stats service (#280)
    Storing Clustering Count in DB through count-stats service

--- eva-accession-clustering/src/main/java/uk/ac/ebi/eva/accession/clustering/configuration/batch/listeners/ListenersConfiguration.java
+++ eva-accession-clustering/src/main/java/uk/ac/ebi/eva/accession/clustering/configuration/batch/listeners/ListenersConfiguration.java
@@ -1,7 +1,9 @@
 package uk.ac.ebi.eva.accession.clustering.configuration.batch.listeners;
 
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 import uk.ac.ebi.eva.accession.clustering.batch.listeners.ClusteringCounts;
 import uk.ac.ebi.eva.accession.clustering.batch.listeners.ClusteringProgressListener;
 import uk.ac.ebi.eva.accession.clustering.parameters.InputParameters;
@@ -13,12 +15,17 @@ public class ListenersConfiguration {
 
     @Bean(PROGRESS_LISTENER)
     public ClusteringProgressListener clusteringProgressListener(InputParameters parameters,
-                                                                 ClusteringCounts clusteringCounts) {
-        return new ClusteringProgressListener(parameters.getChunkSize(), clusteringCounts);
+                                                                 ClusteringCounts clusteringCounts, RestTemplate restTemplate) {
+        return new ClusteringProgressListener(parameters, clusteringCounts, restTemplate);
     }
 
     @Bean
     public ClusteringCounts importCounts() {
         return new ClusteringCounts();
+    }
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
     }
 }
--- eva-accession-clustering/src/main/java/uk/ac/ebi/eva/accession/clustering/parameters/InputParameters.java
+++ eva-accession-clustering/src/main/java/uk/ac/ebi/eva/accession/clustering/parameters/InputParameters.java
@@ -30,6 +30,8 @@ public class InputParameters {
 
     private boolean forceRestart;
 
+    private String countStatsUrl;
+
     public String getVcf() {
         return vcf;
     }
@@ -70,12 +72,21 @@ public class InputParameters {
         this.forceRestart = forceRestart;
     }
 
+    public String getCountStatsUrl() {
+        return countStatsUrl;
+    }
+
+    public void setCountStatsUrl(String countStatsUrl) {
+        this.countStatsUrl = countStatsUrl;
+    }
+
     public JobParameters toJobParameters() {
         return new JobParametersBuilder()
                 .addString("assemblyAccession", assemblyAccession)
                 .addString("projectAccession", projectAccession)
                 .addString("vcf", vcf)
                 .addLong("chunkSize", (long) chunkSize, false)
+                .addString("countStatsUrl", countStatsUrl)
                 .toJobParameters();
     }
 }
