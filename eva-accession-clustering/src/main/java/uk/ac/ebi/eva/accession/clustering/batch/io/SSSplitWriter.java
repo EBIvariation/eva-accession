20edb4bbd181add7d5c1156b0d3e6600cb800f86
sundarvenkata-ebi <sundarvenkata@ebi.ac.uk>
2022-05-26 15:31:10 +0100

    Provide a note on how to use the SSSplitWriter

--- eva-accession-clustering/src/main/java/uk/ac/ebi/eva/accession/clustering/batch/io/SSSplitWriter.java
+++ eva-accession-clustering/src/main/java/uk/ac/ebi/eva/accession/clustering/batch/io/SSSplitWriter.java
@@ -82,6 +82,16 @@ public class SSSplitWriter implements ItemWriter<SubmittedVariantEntity> {
     }
 
     @Override
+    /**
+     * This method can accept a list of submitted variant entities that share duplicate IDs with other
+     * submitted variant entities. It is not necessary to provide the duplicates themselves.
+     * for ex: if Submitted variant entities:
+     * sve1 and sve2 share the same ID ss1
+     * sve3 and sve4 share the same ID ss2
+     * It is sufficient to provide just sve1 and sve3 to this method.
+     * Providing duplicates is harmless but may make the writer inefficient
+     * since it will result in extra queries to the database.
+     */
     public void write(@Nonnull List<? extends SubmittedVariantEntity> submittedVariantEntities)
             throws MongoBulkWriteException, AccessionCouldNotBeGeneratedException, InstantiationException,
             IllegalAccessException {
