d6724c96c5c48c73a26c197c711897e0da7f46d8
sundarvenkata-EBI <sundarvenkata@ebi.ac.uk>
2022-10-23 08:00:49 +0100

    EVA-3026 - Move clustering counts script from EVA-2770 and EVA-2750 to release automation (#372)
    Move scripts from EVA-2770 and EVA-2550 to release automation
    * Update vcf_files category extension
    * Use different parsing functionality to parse remapped logs

---
+++ eva-accession-release-automation/gather_clustering_counts/bash/count_rs_for_release.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+species_dir=$1
+idtype=$2
+species_name=$(basename "$species_dir")
+
+
+for ASSEMBLY_DIR in "${species_dir}"/GC*
+do
+    ASSEMBLY=$(basename "$ASSEMBLY_DIR")
+    zcat  "${species_dir}"/"$ASSEMBLY"/"${ASSEMBLY}"_"${idtype}"_ids_with_genbank.vcf.gz | grep -v '^#' | awk -v assembly="$ASSEMBLY" '{print $3" "assembly}'
+done | sort > tmp_"${species_name}"_"${idtype}"_rsid_sorted
+
+cat tmp_"${species_name}"_"${idtype}"_rsid_sorted \
+    | awk '{if (current_rsid != $1){for (a in assemblies){printf " %s",a} print ""; delete assemblies; current_rsid=$1 }; assemblies[$2]=1} END{for (a in assemblies){printf " %s",a}}'  \
+    | grep -v '^$' | sort | uniq -c | sort -nr > "${species_name}"_count_"${idtype}"_rsid.log
+
+rm  tmp_"${species_name}"_"${idtype}"_rsid_sorted
