086b7169476a77e983c77bf42354ddcadb5daba5
tcezard <tcezard@ebi.ac.uk>
2024-03-13 11:41:45 +0000

    Add taxonomy information to view per assembly and assembly information in view per taxonomy

--- eva-accession-release-automation/gather_clustering_counts/release_count_models.py
+++ eva-accession-release-automation/gather_clustering_counts/release_count_models.py
@@ -8,18 +8,23 @@ metadata = MetaData(schema="eva_stats")
 Base = declarative_base(metadata=metadata)
 
 
+def _pluralise(name):
+    """Only works with name ending in Y"""
+    return f'{name[:-1]}ies'
+
+
 drop_view_template = """DROP VIEW eva_stats.release_rs_count_per_{aggregate}"""
 
 
 create_view_template = """CREATE VIEW eva_stats.release_rs_count_per_{aggregate} AS
 WITH count_per_{aggregate} AS (
-    SELECT {aggregate}, rs_type, release_version, SUM(count) AS count 
+    SELECT {aggregate}, rs_type, release_version, ARRAY_AGG(DISTINCT {source}) AS {_pluralise(source)}, SUM(count) AS count 
     FROM eva_stats.release_rs_count_category cc 
     JOIN eva_stats.release_rs_count c ON c.rs_count_id=cc.rs_count_id 
     GROUP BY {aggregate}, release_version, rs_type
 )
 SELECT current.{aggregate} AS {aggregate}, current.rs_type AS rs_Type, current.release_version AS release_version, 
-       current.count AS count, coalesce(current.count-previous.count, 0) as new 
+       current.{_pluralise(source)} as {_pluralise(source)} current.count AS count, coalesce(current.count-previous.count, 0) as new 
 FROM count_per_{aggregate} AS current
 LEFT JOIN count_per_{aggregate} AS previous
 ON current.release_version=previous.release_version+1 AND current.{aggregate}=previous.{aggregate} AND current.rs_type=previous.rs_type;
@@ -27,14 +32,14 @@ ON current.release_version=previous.release_version+1 AND current.{aggregate}=pr
 
 
 def create_views_from_sql(engine):
-    for t in ['taxonomy', 'assembly']:
+    for aggregate, source in [('taxonomy', 'assembly'), ('assembly', 'taxonomy')]:
         with engine.begin() as conn:
             try:
-                conn.execute(text(drop_view_template.format(aggregate=t)))
+                conn.execute(text(drop_view_template.format(aggregate=aggregate)))
             except sqlalchemy.exc.ProgrammingError:
                 pass
         with engine.begin() as conn:
-            conn.execute(text(create_view_template.format(aggregate=t)))
+            conn.execute(text(create_view_template.format(aggregate=aggregate, source=source)))
 
 
 class RSCountCategory(Base):
