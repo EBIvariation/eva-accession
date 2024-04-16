9b65aae7380be1b625a4a67a5ef9e1572543ee1a
tcezard <tcezard@ebi.ac.uk>
2024-04-16 13:45:16 +0100

    Make script compatible with macos tooling

--- eva-accession-release-automation/gather_clustering_counts/bash/count_rs_for_all_files.sh
+++ eva-accession-release-automation/gather_clustering_counts/bash/count_rs_for_all_files.sh
@@ -20,14 +20,14 @@ do
     if [[ ${INPUT} == *.vcf.gz ]]
     then
         # There are sometime multiple rs (separated by ;) in one line that needs to be split across multiple lines
-        zcat  "${INPUT}" | grep -v '^#' | awk '{gsub(";","\n",$3); print $3}' | awk -v annotation="${ASSEMBLY}-${SC_NAME}-${TYPE}" '{print $0" "annotation}' > ${OUTPUT}
+        gzip -d -c  "${INPUT}" | grep -v '^#' | awk '{gsub(";","\n",$3); print $3}' | awk -v annotation="${ASSEMBLY}-${SC_NAME}-${TYPE}" '{print $0" "annotation}' > ${OUTPUT}
     elif [[ ${INPUT} == *_unmapped_ids.txt.gz ]]
     then
         SC_NAME=$(basename $(dirname ${INPUT}));
         OUTPUT=tmp_${SC_NAME}_unmapped.txt
-        zcat  "${INPUT}" | grep -v '^#' | awk -v annotation="Unmapped-${SC_NAME}-unmapped" '{print $1" "annotation}' > ${OUTPUT}
+        gzip -d -c "${INPUT}" | grep -v '^#' | awk -v annotation="Unmapped-${SC_NAME}-unmapped" '{print $1" "annotation}' > ${OUTPUT}
     else
-        zcat  "${INPUT}" | grep -v '^#' | awk -v annotation="${ASSEMBLY}-${SC_NAME}-${TYPE}" '{print $1" "annotation}' > ${OUTPUT}
+        gzip -d -c "${INPUT}" | grep -v '^#' | awk -v annotation="${ASSEMBLY}-${SC_NAME}-${TYPE}" '{print $1" "annotation}' > ${OUTPUT}
     fi
     ALL_TMP_OUTPUT=$OUTPUT" "$ALL_TMP_OUTPUT
 done
