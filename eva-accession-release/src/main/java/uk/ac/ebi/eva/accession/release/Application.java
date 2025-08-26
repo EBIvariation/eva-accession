41219053b7c727c6fa83495961431de6ef76d17d
nitin-ebi <79518737+nitin-ebi@users.noreply.github.com>
2025-08-26 15:55:27 +0100

    use in-memory job repository for spring batch (#478)

--- eva-accession-release/src/main/java/uk/ac/ebi/eva/accession/release/Application.java
+++ eva-accession-release/src/main/java/uk/ac/ebi/eva/accession/release/Application.java
@@ -18,10 +18,17 @@ package uk.ac.ebi.eva.accession.release;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.ConfigurableApplicationContext;
 
 @EnableBatchProcessing
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        DataSourceAutoConfiguration.class,
+        DataSourceTransactionManagerAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class
+})
 public class Application {
 
     public static void main(String[] args) {
